8c8b02acb3d803175f882fb1233e72b426473706
DimaKupriyanow <123324626+DimaKupriyanow@users.noreply.github.com>
2023-04-04 18:55:55 +0300

    sprint

---
+++ script.js
@@ -0,0 +1,41 @@
+const changeButton = document.querySelector('.profile__button_change');
+const moreInfoPopup = document.querySelector('.popup_type_more-info');
+const moreInfoClose = moreInfoPopup.querySelector('.popup__close');
+const inputName = moreInfoPopup.querySelector('.popup__input_name');
+const inputClass = moreInfoPopup.querySelector('.popup__input_class');
+const saveInfoPopupSubmit = moreInfoPopup.querySelector('.popup__button_submit');
+const saveInfoPopupForm = moreInfoPopup.querySelector('.popup__container');
+const infoName = document.querySelector('.profile__title');
+const infoClass = document.querySelector('.profile__subtitle');
+
+
+changeButton.addEventListener('click', () => {
+    moreInfoPopup.classList.add('popup_opened');
+    inputName.value = infoName.textContent;
+    inputClass.value = infoClass.textContent;
+});
+moreInfoClose.addEventListener('click', () => {
+    moreInfoPopup.classList.remove('popup_opened');
+    inputName.value = infoName.textContent;
+    inputClass.value = infoClass.textContent;
+});
+
+saveInfoPopupForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    infoName.textContent = inputName.value;
+
+    infoClass.textContent = inputClass.value;
+
+    moreInfoPopup.classList.remove('popup_opened');
+
+})
+  
+
+
+
+
+
+
+
+
