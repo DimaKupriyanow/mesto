3bbcd3067dd2880ffee88c477cc34e64d6fb8585
DimaKupriyanow <123324626+DimaKupriyanow@users.noreply.github.com>
2023-08-28 18:17:23 +0300

    исправлены ошибки

--- components/Popup.js
+++ components/Popup.js
@@ -2,31 +2,36 @@
 export class Popup {
   constructor(popupElement) {
     this._popupElement = popupElement;
-    this._buttonsClose = this._popupElement.querySelector(".popup__close");
+    this._buttonsClose = popupElement.querySelector(".popup__close");
+    this._closePopupEsc = this._closePopupEsc.bind(this);
   }
 
   open() {
     this._popupElement.classList.add("popup_opened");
+    document.addEventListener("keydown", this._closePopupEsc); 
   }
 
   close() {
     this._popupElement.classList.remove("popup_opened");
+    document.removeEventListener("keydown", this._closePopupEsc);
   }
 
   _handleEscClose(evt) {
     if (evt.key === "Escape") {
       this.close();
+      } 
     }
-  }
+
+    _closePopupEsc() {
+      document.addEventListener("keydown", (evt) => {
+        this._handleEscClose(evt);
+      })
+    }
   
   setEventListeners() {
     this._buttonsClose
     .addEventListener("click", () => {
       this.close();
-    });
-
-    document.addEventListener("keydown", (evt) => {
-      this._handleEscClose(evt);
     });
 
     this._popupElement.addEventListener("click", (evt) => {
